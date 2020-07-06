2a53510e54d7505a29013d7688ac988821dcf029
Gianluca Pagano <gpagano@frro.utn.edu.ar>
2020-07-06 20:51:34 -0300

    Primera Subida

---
+++ Data.Database/UsuarioAdapter.cs
@@ -0,0 +1,182 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Entities;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Data.Database
+{
+    public class UsuarioAdapter:Adapter
+    {
+        #region DatosEnMemoria
+        // Esta región solo se usa en esta etapa donde los datos se mantienen en memoria.
+        // Al modificar este proyecto para que acceda a la base de datos esta será eliminada
+        private static List<Usuario> _Usuarios;
+
+        private static List<Usuario> Usuarios
+        {
+            get
+            {
+                if (_Usuarios == null)
+                {
+                    _Usuarios = new List<Business.Entities.Usuario>();
+                    Business.Entities.Usuario usr;
+                    usr = new Business.Entities.Usuario();
+                    usr.ID = 1;
+                    usr.State = Business.Entities.BusinessEntity.States.Unmodified;
+                    usr.Nombre = "Casimiro";
+                    usr.Apellido = "Cegado";
+                    usr.NombreUsuario = "casicegado";
+                    usr.Clave = "miro";
+                    usr.Email = "casimirocegado@gmail.com";
+                    usr.Habilitado = true;
+                    _Usuarios.Add(usr);
+
+                    usr = new Business.Entities.Usuario();
+                    usr.ID = 2;
+                    usr.State = Business.Entities.BusinessEntity.States.Unmodified;
+                    usr.Nombre = "Armando Esteban";
+                    usr.Apellido = "Quito";
+                    usr.NombreUsuario = "aequito";
+                    usr.Clave = "carpintero";
+                    usr.Email = "armandoquito@gmail.com";
+                    usr.Habilitado = true;
+                    _Usuarios.Add(usr);
+
+                    usr = new Business.Entities.Usuario();
+                    usr.ID = 3;
+                    usr.State = Business.Entities.BusinessEntity.States.Unmodified;
+                    usr.Nombre = "Alan";
+                    usr.Apellido = "Brado";
+                    usr.NombreUsuario = "alanbrado";
+                    usr.Clave = "abrete sesamo";
+                    usr.Email = "alanbrado@gmail.com";
+                    usr.Habilitado = true;
+                    _Usuarios.Add(usr);
+
+                }
+                return _Usuarios;
+            }
+        }
+        #endregion
+
+        public List<Usuario> GetAll()
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdUsuarios = new SqlCommand("Select * from usuarios", sqlConn);
+                SqlDataReader drUsuarios = cmdUsuarios.ExecuteReader();
+                while (drUsuarios.Read())
+                {
+                    Usuario usr = new Usuario();
+                    usr.ID = (int)drUsuarios["id_usuario"];
+                    usr.NombreUsuario = (string)drUsuarios["nombre_usuario"];
+                    usr.Clave = (string)drUsuarios["clave"];
+                    usr.Habilitado = (bool)drUsuarios["habilitado"];
+                    usr.Nombre = (string)drUsuarios["nombre"];
+                    usr.Apellido = (string)drUsuarios["apellido"];
+                    usr.Email = (string)drUsuarios["email"];
+                    usuarios.Add(usr);
+                }
+                drUsuarios.Close();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada =
+                    new Exception("Error al recuperar lista de usuarios", Ex);
+                throw ExcepcionManejada;
+            }
+            finally 
+            {
+                this.CloseConnection();
+            }
+            return usuarios;
+        }
+
+        public Business.Entities.Usuario GetOne(int ID)
+        {
+            Usuario usr = new Usuario();
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdUsuarios = new SqlCommand("Select * from usuarios where id_usuario @id", sqlConn);
+                cmdUsuarios.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                SqlDataReader drUsuarios = cmdUsuarios.ExecuteReader();
+                if (drUsuarios.Read())
+                {
+                    usr.ID = (int)drUsuarios["id_usuario"];
+                    usr.NombreUsuario = (string)drUsuarios["nombre_usuario"];
+                    usr.Clave = (string)drUsuarios["clave"];
+                    usr.Habilitado = (bool)drUsuarios["habilitado"];
+                    usr.Nombre = (string)drUsuarios["nombre"];
+                    usr.Apellido = (string)drUsuarios["apellido"];
+                    usr.Email = (string)drUsuarios["email"];
+                }
+                drUsuarios.Close();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada =
+                    new Exception("Error al recuperar datos de usuario", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return usr;
+        }
+
+        
+
+        public void Delete(int ID)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdDelete = new SqlCommand("delete usuarios where id_usuario=@id", sqlConn);
+                cmdDelete.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                cmdDelete.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada =
+                    new Exception("Error al eliminar usuario", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        public void Save(Usuario usuario)
+        {
+            if (usuario.State == BusinessEntity.States.New)
+            {
+                int NextID = 0;
+                foreach (Usuario usr in Usuarios)
+                {
+                    if (usr.ID > NextID)
+                    {
+                        NextID = usr.ID;
+                    }
+                }
+                usuario.ID = NextID + 1;
+                Usuarios.Add(usuario);
+            }
+            else if (usuario.State == BusinessEntity.States.Deleted)
+            {
+                this.Delete(usuario.ID);
+            }
+            else if (usuario.State == BusinessEntity.States.Modified)
+            {
+                Usuarios[Usuarios.FindIndex(delegate(Usuario u) { return u.ID == usuario.ID; })]=usuario;
+            }
+            usuario.State = BusinessEntity.States.Unmodified;            
+        }
+    }
+}
